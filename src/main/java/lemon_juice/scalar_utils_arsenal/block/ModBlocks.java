80884f347b95c9d4468e441fe88f48591dc43405
Lemon-Juiced <tbrotheawesome@gmail.com>
2022-08-16 20:13:07 -0400

    Raw Ore Blocks Got Armor Ready For Texture Implementation

--- src/main/java/lemon_juice/scalar_utils_arsenal/block/ModBlocks.java
+++ src/main/java/lemon_juice/scalar_utils_arsenal/block/ModBlocks.java
@@ -26,6 +26,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> ALTARUS_BLOCK = registerBlock("altarus_block", () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SCALAR_UTILS_TAB);
     public static final RegistryObject<Block> BLUTSTEIN_BLOCK = registerBlock("blutstein_block", () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SCALAR_UTILS_TAB);
     public static final RegistryObject<Block> MITHRIL_BLOCK = registerBlock("mithril_block", () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SCALAR_UTILS_TAB);
+    public static final RegistryObject<Block> RAW_ADAMANTINE_BLOCK = registerBlock("raw_adamantine_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SCALAR_UTILS_TAB);
+    public static final RegistryObject<Block> RAW_ALTARUS_BLOCK = registerBlock("raw_altarus_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SCALAR_UTILS_TAB);
+    public static final RegistryObject<Block> RAW_MITHRIL_BLOCK = registerBlock("raw_mithril_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SCALAR_UTILS_TAB);
     public static final RegistryObject<Block> TARTARITE_BLOCK = registerBlock("tartarite_block", () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.SCALAR_UTILS_TAB);
 
     /* Nether Ores */
--- src/main/java/lemon_juice/scalar_utils_arsenal/world/feature/ModPlacedFeatures.java
+++ src/main/java/lemon_juice/scalar_utils_arsenal/world/feature/ModPlacedFeatures.java
@@ -16,22 +16,22 @@ public class ModPlacedFeatures {
 
     public static final RegistryObject<PlacedFeature> ADAMANTINE_ORE_PLACED = PLACED_FEATURES.register("adamantine_ore_placed",
             () -> new PlacedFeature(ModConfiguredFeatures.ADAMANTINE_ORE.getHolder().get(),
-                    commonOrePlacement(7, // VeinsPerChunk
-                            HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.aboveBottom(0)))));
+                    commonOrePlacement(8, // Veins Per Chunk
+                            HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.aboveBottom(32)))));
 
     public static final RegistryObject<PlacedFeature> ALTARUS_ORE_PLACED = PLACED_FEATURES.register("altarus_ore_placed",
             () -> new PlacedFeature(ModConfiguredFeatures.ALTARUS_ORE.getHolder().get(),
-                    commonOrePlacement(7, // VeinsPerChunk
-                            HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.aboveBottom(0)))));
+                    commonOrePlacement(8, // Veins Per Chunk
+                            HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.aboveBottom(32)))));
 
     public static final RegistryObject<PlacedFeature> MITHRIL_ORE_PLACED = PLACED_FEATURES.register("mithril_ore_placed",
             () -> new PlacedFeature(ModConfiguredFeatures.ALTARUS_ORE.getHolder().get(),
-                    commonOrePlacement(7, // VeinsPerChunk
-                            HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.aboveBottom(32)))));
+                    commonOrePlacement(12, // Veins Per Chunk
+                            HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-64), VerticalAnchor.aboveBottom(64)))));
 
 
     public static final RegistryObject<PlacedFeature> NETHER_BLUTSTEIN_ORE_PLACED = PLACED_FEATURES.register("nether_blutstein_ore_placed",
-            () -> new PlacedFeature(ModConfiguredFeatures.NETHER_BLUTSTEIN_ORE.getHolder().get(), commonOrePlacement(7, // VeinsPerChunk
+            () -> new PlacedFeature(ModConfiguredFeatures.NETHER_BLUTSTEIN_ORE.getHolder().get(), commonOrePlacement(7, // Veins Per Chunk
                     HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(80)))));
 
 
